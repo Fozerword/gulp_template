0cd0c43622236cae62d4993ab636ab5989bc187c
Rostyslav <akovkvlv798@gmail.com>
2023-04-26 21:18:23 -0400

    version 0.0.6

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,10 +1,12 @@
 import replace from "gulp-replace"; // search and replace
 import plumber from "gulp-plumber"; // processing mistake
 import notify from "gulp-notify"; // message (hints)
+import browsersync from "browser-sync"; // local server
 //Export object
 
 export const plugins =  {                  // <----------- this object
     replace: replace,
     plumber: plumber,
-    notify: notify
+    notify: notify,
+    browsersync: browsersync
 }
