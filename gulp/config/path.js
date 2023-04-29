0737456b4c529f535aefa691b43ba344709f6cb3
Rostyslav <akovkvlv798@gmail.com>
2023-04-28 23:49:28 -0400

    version 0.0.7

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,22 +6,26 @@ const srcFolder = './src';
 
 export const path = {
     build: {
+        js: `${buildFolder}/js/`,
         css: `${buildFolder}/css/`,
         html:  `${buildFolder}/`,
         files: `${buildFolder}/files/`,
-        img: `${buildFolder}/img/`
+        images: `${buildFolder}/img/`
     },  //folder results
     src: {
+        js: `${srcFolder}/js/app.js`,
         scss: `${srcFolder}/scss/style.scss`,
         html:  `${srcFolder}/*.html`,
         files: `${srcFolder}/files**/*.*`,
-        img: `${srcFolder}/img/*.*`
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`
     },
     watch: {
+        js: `${srcFolder}/js/**/*.js`,
         scss: `${srcFolder}/scss/**/*.scss`,
         html:  `${srcFolder}/**/*.html`,
         files: `${srcFolder}/files**/*.*`,
-        img: `${srcFolder}/img/*.*`
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`
     }, //check progect
     clean: buildFolder,
     buildFolder: buildFolder,
--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -2,11 +2,13 @@ import replace from "gulp-replace"; // search and replace
 import plumber from "gulp-plumber"; // processing mistake
 import notify from "gulp-notify"; // message (hints)
 import browsersync from "browser-sync"; // local server
+import newer from "gulp-newer"; //Check update
 //Export object
 
 export const plugins =  {                  // <----------- this object
     replace: replace,
     plumber: plumber,
     notify: notify,
-    browsersync: browsersync
+    browsersync: browsersync,
+    newer: newer
 }
